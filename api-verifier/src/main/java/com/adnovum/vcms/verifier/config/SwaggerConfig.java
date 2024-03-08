2cff11a878a29024c05e56f7e10401a2a01b5228
Balazs Hosszu <balazs.hosszu@adnovum.hu>
2024-03-08 18:15:14 +0100

    Move to Springfox for Swagger generation
    Add Springfox Spring Starter, enabled by default, to
    generate Swagger docs.
    Some Springfox libraries were already in use,
    preferring this tool over the conflicting swagger.ui
    library.

---
+++ api-verifier/src/main/java/com/adnovum/vcms/verifier/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.adnovum.vcms.verifier.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.PathSelectors;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.OAS_30).select()
+				.apis(RequestHandlerSelectors.basePackage("com.adnovum.vcms.verifier.controller")).paths(PathSelectors.any())
+				.build().apiInfo(new ApiInfoBuilder().title("VCMS Verifier API")
+						.description("API to request verification " + "and user data.").version("1.2.1").build());
+	}
+
+}
+
+
